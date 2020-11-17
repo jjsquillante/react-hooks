d97d0a414c84d8f92ee411da7539325ec38ce53a
James Squillante <jjsquillante@gmail.com>
2020-11-16 21:02:05 -0600

    exercise - 01

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -1,29 +1,28 @@
 // useState: greeting
 // http://localhost:3000/isolated/exercise/01.js
 
-import * as React from 'react'
+import React, { useState } from 'react';
 
-function Greeting() {
-  // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+function Greeting({ initialName = '' }) {
+  const [name, setName] = useState(initialName);
 
-  function handleChange(event) {
-    // 🐨 update the name here based on event.target.value
+  function handleChange(e) {
+    setName(e.target.value);
   }
 
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input onChange={handleChange} id="name" value={name} />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
-  )
+  );
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName="James" />;
 }
 
-export default App
+export default App;
